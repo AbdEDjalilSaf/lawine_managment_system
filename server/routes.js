7d86f54674753adf9748b854fe7aa9423332b74e
ReDhaBenKortbi <ridhabenkortbi@gmail.com>
2024-11-08 11:07:06 +0100

    fix: Google auth not working #12

--- server/routes.js
+++ server/routes.js
@@ -25,13 +25,8 @@ authRouter.get(
 authRouter.get(
   "/google/callback",
   passport.authenticate("google", {
-    failureRedirect: "http://localhost:5137/login", // In case of failure
+    failureRedirect: "http://localhost:3000/login", // in case of failure
+    successRedirect: "http://localhost:3000/home", // In case of sucess
     session: true, // Keep the session (persistent login)
-  }),
-  (req, res) => {
-    console.log("Response from google callback successfull");
-    console.log("is Request authenticated now: ", req.isAuthenticated());
-    // Successful authentication, redirect to the frontend /profile page with data
-    res.redirect(`http://localhost:5173/profile`);
-  }
+  })
 );
